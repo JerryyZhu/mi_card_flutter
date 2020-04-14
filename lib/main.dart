2fe9b8c12b7396800589d3b5a52663569d35b9f5
JerryyZhu <jerrybzhu@gmail.com>
2020-04-15 00:47:03 +1000

    added fonts / created layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,78 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/minion.jpg'),
+              ),
+              Text('Jerry Zhu',
+                  style: TextStyle(
+                      fontSize: 40.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Pacifico')),
+              Text('Software Developer',
+                  style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.teal.shade100,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 2.5,
+                      fontFamily: 'Source Sans Pro')),
+              SizedBox(
+                  height: 20.0,
+                  width: 150.0,
+                  child: Divider(color: Colors.teal.shade200)),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(Icons.phone, color: Colors.teal),
+                  title: Text('+12 3456 7890',
+                      style: TextStyle(
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 20.0,
+                          color: Colors.teal.shade900)),
+                ),
+              ),
+              Card(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(Icons.email, color: Colors.teal),
+                    title: Text('jerrybzhu@gmail.com',
+                        style: TextStyle(
+                            fontFamily: 'Source Sans Pro',
+                            fontSize: 20.0,
+                            color: Colors.teal.shade900)),
+                  )),
+            ])),
       ),
-    ),
-  );
+    );
+  }
 }
+
+//Row(children: <Widget>[
+//Icon(Icons.phone, color: Colors.teal),
+//SizedBox(
+//width: 10.0,
+//),
+//Text('+12 3456 7890',
+//style: TextStyle(
+//fontFamily: 'Source Sans Pro',
+//fontSize: 20.0,
+//color: Colors.teal.shade900))
+//]),
